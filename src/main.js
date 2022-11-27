7a6a7b87d35ffce8b8e7cf5b3c1939e7203b3a1c
gabrielkf <gabrielkf@gmail.com>
2022-11-27 15:39:02 -0300

    Setup

---
+++ src/main.js
@@ -0,0 +1,9 @@
+/**
+ * @param brackets string
+ * @return boolean
+ */
+function areBracketsBalanced(brackets) {
+    return true;
+}
+
+module.exports = areBracketsBalanced;
---
+++ tests/main.test.js
@@ -0,0 +1,5 @@
+const areBracketsBalanced = require('../src/main');
+
+test('areBracketsBalanced', () => {
+    expect(areBracketsBalanced()).toBe(true);
+});
