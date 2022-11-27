a2aaabff52f4f3dc02391b38fd6b581eb22dc345
gabrielkf <gabrielkf@gmail.com>
2022-11-27 16:45:54 -0300

    Unit test against test cases

--- tests/main.test.js
+++ tests/main.test.js
@@ -1,5 +1,17 @@
 const areBracketsBalanced = require('../src/main');
 
-test('areBracketsBalanced', () => {
-    expect(areBracketsBalanced()).toBe(true);
+const TEST_CASES = [
+    { bracketString: '(){}[]', isValid: true },
+    { bracketString: '[{()}](){}', isValid: true },
+    { bracketString: '[]{()', isValid: false },
+    { bracketString: '[{)]', isValid: false },
+];
+
+describe('areBracketsBalanced function', () => {
+    test.each(TEST_CASES)(
+        '$bracketString is valid: $isValid',
+        ({ bracketString, isValid }) => {
+            expect(areBracketsBalanced(bracketString)).toBe(isValid);
+        }
+    );
 });
