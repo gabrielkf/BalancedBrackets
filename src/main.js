28bb868df724c2d117f1776110698b422c5be6d7
gabrielkf <gabrielkf@gmail.com>
2022-11-27 17:05:11 -0300

    Implement function; tests pass

--- src/main.js
+++ src/main.js
@@ -1,9 +1,29 @@
+const closing = {
+    '(': ')',
+    '[': ']',
+    '{': '}',
+};
+
 /**
- * @param brackets string
+ * @param bracketsString string
  * @return boolean
  */
-function areBracketsBalanced(brackets) {
-    return true;
+function areBracketsBalanced(bracketsString) {
+    const size = bracketsString.length;
+    const opened = [];
+
+    for (let c = 0; c < size; c++) {
+        let bracket = bracketsString[c];
+        if (closing[bracket]) {
+            opened.push(bracket);
+        } else if (opened.length === 0) {
+            return false;
+        } else if (closing[opened.pop()] !== bracket) {
+            return false;
+        }
+    }
+
+    return opened.length === 0;
 }
 
 module.exports = areBracketsBalanced;
